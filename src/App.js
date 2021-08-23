f1f5ab387bd83a7a6dbd14c9e98acf5676b423e3
Saber Mnasri <saber.mnasri@hotmail.com>
2021-08-23 14:20:15 +0200

    correction 16.8.1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 16.8
+          <=== Learn React 16.8.1 coorectif ===>
         </a>
       </header>
     </div>
