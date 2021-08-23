24b450951f58ba6161083906cc9919011fa44812
Saber Mnasri <saber.mnasri@hotmail.com>
2021-08-23 14:12:20 +0200

    add iss53 to app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 16.8
+          Learn React iss53
         </a>
       </header>
     </div>
